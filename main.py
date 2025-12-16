aba60affb6fa4576d86efe84322d52768bc48ce3
Chedadeepu Saiteja <chedadeepusaitejajobs@gmail.com>
2025-12-15 23:19:21 -0500

    Day2: Added student GET and POST API's

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# ---------- Model ----------
+class Student(BaseModel):
+    id: int
+    name: str
+    course: str
+
+# ---------- Fake Database ----------
+students = []
+
+# ---------- Routes ----------
 @app.get("/")
-def read_root():
-    return {"message": "GenAI Backend Journey Started"}
+def root():
+    return {"message": "Day 2: Student API"}
+
+@app.get("/students")
+def get_students():
+    return students
+
+@app.post("/students")
+def create_student(student: Student):
+    students.append(student)
+    return {"message": "Student added successfully", "student": student}
