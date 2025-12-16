de43b4e4b1f23409cb56a59bdc0078b6be4acff9
Chedadeepu Saiteja <chedadeepusaitejajobs@gmail.com>
2025-12-15 23:59:32 -0500

    Day 3 : completed CRUD APIs with update and delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,7 +15,7 @@ students = []
 # ---------- Routes ----------
 @app.get("/")
 def root():
-    return {"message": "Day 2: Student API"}
+    return {"message": "Day 3: Complete CRUD API"}
 
 @app.get("/students")
 def get_students():
@@ -23,5 +23,25 @@ def get_students():
 
 @app.post("/students")
 def create_student(student: Student):
+    # Prevent duplicate ID
+    for s in students:
+        if s.id == student.id:
+            raise HTTPException(status_code=400, detail="Student with this ID already exists")
     students.append(student)
     return {"message": "Student added successfully", "student": student}
+
+@app.put("/students/{student_id}")
+def update_student(student_id: int, updated_student: Student):
+    for index, s in enumerate(students):
+        if s.id == student_id:
+            students[index] = updated_student
+            return {"message": "Student updated successfully", "student": updated_student}
+    raise HTTPException(status_code=404, detail="Student not found")
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    for index, s in enumerate(students):
+        if s.id == student_id:
+            deleted = students.pop(index)
+            return {"message": "Student deleted successfully", "student": deleted}
+    raise HTTPException(status_code=404, detail="Student not found")
